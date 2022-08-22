9f575c7e4712966581b4368a7909f52c790f77fd
neosao services <testing.neosaoservices@gmail.com>
2022-08-22 14:40:02 +0530

    first commit

---
+++ project/app/Http/Controllers/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('admin.auth.login');
+    }
+    public function reset(Request $r)
+    {
+        return view('admin.auth.verify');
+    }
+
+}
